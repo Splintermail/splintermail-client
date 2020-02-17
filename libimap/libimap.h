0b68ec79aba66ec82b93399afb979cef896f27c3
Splintermail Dev <dev@splintermail.com>
2020-02-17 10:57:12 -0700

    prepare for libimap

---
+++ libimap/libimap.h
@@ -0,0 +1,12 @@
+#ifndef LIBIMAP_H
+#define LIBIMAP_H
+
+#include "expression.h"
+#include "extension.h"
+#include "parse.h"
+#include "read.h"
+#include "scan.h"
+#include "util.h"
+#include "write.h"
+
+#endif // LIBIMAP_H
