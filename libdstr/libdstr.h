c0826c15b91f92c25e12d44e04fcddd9e61e8150
Splintermail Dev <dev@splintermail.com>
2020-02-17 09:18:23 -0700

    prepare for libdstr

---
+++ libdstr/libdstr.h
@@ -0,0 +1,14 @@
+#ifndef LIBDSTR_H
+#define LIBDSTR_H
+
+#include "common.h"
+#include "fileops.h"
+#include "hashmap.h"
+#include "json.h"
+#include "jsw_atree.h"
+#include "link.h"
+#include "logger.h"
+#include "opt_parse.h"
+#include "win_compat.h"
+
+#endif // LIBDSTR_H
