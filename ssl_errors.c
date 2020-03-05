ef9d6a71f65e5e55a5a6b1f3b203627e14b10e1b
Splintermail Dev <dev@splintermail.com>
2020-03-05 04:24:12 -0700

    add ssl_errors.c

---
+++ ssl_errors.c
@@ -0,0 +1,14 @@
+#include <openssl/err.h>
+
+#include "ssl_errors.h"
+
+REGISTER_ERROR_TYPE(E_SSL, "SSLERROR");
+
+void trace_ssl_errors(derr_t *e){
+    unsigned long ssl_error;
+    while( (ssl_error = ERR_get_error()) ){
+        char buffer[256];
+        ERR_error_string_n(ssl_error, buffer, sizeof(buffer));
+        TRACE(e, "OpenSSL error: %x\n", FS(buffer));
+    }
+}
