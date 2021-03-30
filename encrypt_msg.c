216bde1874af8e0cf9428abce0b905f33dd142b4
Splintermail Dev <dev@splintermail.com>
2021-03-30 07:44:44 -0600

    rewrite encrypt_msg to use libsmsql

--- encrypt_msg.c
+++ encrypt_msg.c
@@ -10,6 +10,7 @@
 
 #ifdef BUILD_SERVER_CODE
 #include "server/mysql_util/mysql_util.h"
+#include "server/libsmsql.h"
 #include "server/badbadbad_alert.h"
 #endif // BUILD_SERVER_CODE
 
@@ -19,10 +20,6 @@
 
 // this should only have to be 10, but just in case...
 #define MAX_ENCRYPTION_KEYS 32
-
-// user has no keys (non-critical error)
-derr_type_t E_NOKEYS;
-REGISTER_ERROR_TYPE(E_NOKEYS, "NOKEYS");
 
 static derr_t do_encryption(link_t *keys){
     derr_t e = E_OK;
@@ -99,222 +96,121 @@ cu_keys:
 
 #ifdef BUILD_SERVER_CODE
 
-static void trace_sql_error(derr_t *e, MYSQL* sql){
-    if(*mysql_error(sql)){
-        TRACE(e, "%x\n", FS(mysql_error(sql)));
-    }
-}
-
-static derr_t get_user_id(MYSQL* sql, const char* email, unsigned int* uid){
+// just stream stdin to stdout
+/* this is so that encrypt_msg is a well-behaved sieve filter in the case that
+   a user has no keys for their account */
+static derr_t do_copy(void){
     derr_t e = E_OK;
-    // build request
-    DSTR_VAR(req, 256);
-    PROP(&e, FMT(&req, "select user_id from accounts where email = '%x'", FS(email)) );
-    LOG_DEBUG("mysql request to fetch user_id:\n    %x\n", FD(&req));
 
-    // do request
-    int ret = mysql_real_query(sql, req.data, req.len);
-    if(ret != 0){
-        trace_sql_error(&e, sql);
-        ORIG(&e, E_SQL, "mysql_real_query failed");
+    DSTR_VAR(buf, 4096);
+
+    while(true){
+        size_t amnt_read;
+        PROP(&e, dstr_read(0, &buf, 0, &amnt_read) );
+        if(amnt_read == 0){
+            break;
+        }
+
+        // write the encrypted buffer to stdout
+        PROP(&e, dstr_write(1, &buf) );
+        buf.len = 0;
     }
 
-    // get result
-    MYSQL_RES* res = mysql_use_result(sql);
-    if(!res){
-        trace_sql_error(&e, sql);
-        ORIG(&e, E_SQL, "mysql_use_result failed");
-    }
-
-    // make sure that we get at least one result
-    bool found_uid = false;
-
-    // loop through results
-    MYSQL_ROW row;
-    while( (row = mysql_fetch_row(res)) ){
-        found_uid = true;
-        // get length of uid
-        unsigned long *lens = mysql_fetch_lengths(res);
-        // wrap uid in a dstr
-        dstr_t uidstr;
-        DSTR_WRAP(uidstr, row[0], lens[0], 0);
-        // convert to number
-        PROP_GO(&e, dstr_tou(&uidstr, uid, 10), cleanup_res);
-    }
-    // make sure we exited the loop without an error
-    if(*mysql_error(sql)){
-        trace_sql_error(&e, sql);
-        ORIG_GO(&e, E_SQL, "error fetching rows", cleanup_res);
-    }
-
-    if(!found_uid){
-        ORIG_GO(&e, E_INTERNAL, "no such user", cleanup_res);
-    }
-
-cleanup_res:
-    mysql_free_result(res);
     return e;
 }
 
-static derr_t get_keys(MYSQL* sql, unsigned int uid, link_t* keys){
+static derr_t _mysql_encrypt(MYSQL *sql, const dstr_t user){
     derr_t e = E_OK;
-    // build request
-    DSTR_VAR(req, 256);
-    PROP(&e, FMT(&req, "select fingerprint, public_key from devices "
-                    "where user_id = %x", FU(uid)) );
-    LOG_DEBUG("mysql request to fetch fingerprints:\n    %x\n", FD(&req));
 
-    // do request
-    int ret = mysql_real_query(sql, req.data, req.len);
-    if(ret != 0){
-        trace_sql_error(&e, sql);
-        ORIG(&e, E_SQL, "mysql_real_query failed");
+    dstr_t fsid;
+    dstr_t domain;
+    dstr_split2_soft(user, DSTR_LIT("@"), NULL, &fsid, &domain);
+
+    if(dstr_cmp(&domain, &DSTR_LIT("x.splintermail.com")) != 0){
+        TRACE(&e, "user=%x, domain=%x\n", FD(&user), FD(&domain));
+        ORIG(&e, E_INTERNAL, "unknown domain");
     }
 
-    // get result
-    MYSQL_RES* res = mysql_use_result(sql);
-    if(!res){
-        trace_sql_error(&e, sql);
-        ORIG(&e, E_SSL, "mysql_use_result failed");
-    }
+    DSTR_VAR(uuid, SMSQL_UUID_SIZE);
+    PROP(&e, to_uuid(&fsid, &uuid) );
 
-    // loop through results
-    LOG_DEBUG("mysql fingerprint results:\n");
-    MYSQL_ROW row;
-    while( (row = mysql_fetch_row(res)) ){
-        // get lengths of fields
-        unsigned long *lens = mysql_fetch_lengths(res);
+    link_t pems;
+    link_init(&pems);
+    link_t keys;
+    link_init(&keys);
+    link_t *link;
 
-        // create a keypair from the public_key field
-        dstr_t pem;
-        DSTR_WRAP(pem, row[1], lens[1], 0);
+    // returns a list of pem-encoded public keys (smsql_dstr_t's)
+    PROP(&e, list_device_keys(sql, &uuid, &pems) );
+
+    // convert each pem-encoded key into a keypair_t
+    while(!link_list_isempty(&pems)){
+        smsql_dstr_t *pem = CONTAINER_OF(pems.next, smsql_dstr_t, link);
+
+        // read this key from its pem text
         keypair_t *kp;
-        PROP_GO(&e, keypair_from_pubkey_pem(&kp, &pem), loop_end);
+        PROP_GO(&e, keypair_from_pubkey_pem(&kp, &pem->dstr), cu);
 
-        // append key to output list
-        link_list_append(keys, &kp->link);
+        link_list_append(&keys, &kp->link);
 
-        // verify the fingerprint matches
-        dstr_t hexfpr;
-        DSTR_WRAP(hexfpr, row[0], lens[0], 0);
-        LOG_DEBUG("    %x\n", FD(&hexfpr));
-        DSTR_VAR(fpr, FL_FINGERPRINT);
-        PROP_GO(&e, hex2bin(&hexfpr, &fpr), loop_end);
-
-        if(dstr_cmp(&fpr, kp->fingerprint) != 0){
-            ORIG_GO(&e, E_INTERNAL,
-                    "mismatched db vs calculated fingerprint", loop_end);
-        }
+        // free consumed pem key
+        link_list_pop_first(&pems);
+        smsql_dstr_free(&pem);
     }
-loop_end:
-    // if we had an error, we still need to read all the results
-    while( (row = mysql_fetch_row(res)) ) {}
-    // check if we exited the loop with an error
-    if(*mysql_error(sql)){
-        trace_sql_error(&e, sql);
-        ORIG_GO(&e, E_SQL, "error fetching rows", cleanup_res);
-    }
-    // also cleanup if we ran into other errors in the loop
-    PROP_GO(&e, e, cleanup_res);
 
-cleanup_res:
-    mysql_free_result(res);
+    if(link_list_isempty(&keys)){
+        // if there are no keys, just copy stdin to stdout
+        PROP_GO(&e, do_copy(), cu);
+    }else{
+        PROP_GO(&e, do_encryption(&keys), cu);
+    }
+
+cu:
+    while((link = link_list_pop_first(&pems))){
+        smsql_dstr_t *pem = CONTAINER_OF(link, smsql_dstr_t, link);
+        smsql_dstr_free(&pem);
+    }
+
+    while((link = link_list_pop_first(&keys))){
+        keypair_t *kp = CONTAINER_OF(link, keypair_t, link);
+        keypair_free(&kp);
+    }
+
     return e;
 }
 
-static derr_t mysql_encrypt(const dstr_t *debug_sock){
+static derr_t mysql_encrypt(const dstr_t *sock){
     derr_t e = E_OK;
-    // get USER variable (that is, to whom we are encrypting)
-    char* user = getenv("USER");
 
-    // get null-terminated values, for mysql api
-    char *sqlhost = NULL;
-    char *sqluser = NULL;
-    char *sqlpass = NULL;
-    char *sqlsock = NULL;
-    DSTR_VAR(d_sqlhost, 256);
-    DSTR_VAR(d_sqluser, 256);
-    DSTR_VAR(d_sqlpass, 256);
-    DSTR_VAR(d_sqlsock, 256);
-    if(debug_sock){
-        PROP(&e, dstr_copy(debug_sock, &d_sqlsock) );
-        PROP(&e, dstr_null_terminate(&d_sqlsock) );
-        sqlsock = d_sqlsock.data;
-    }else{
-        // load the mysql config from a file
-        DSTR_VAR(conf_text, 256);
-        PROP(&e, dstr_read_file("/etc/encrypt_msg/encrypt_msg.conf", &conf_text) );
-        // parse the file
-        LIST_VAR(dstr_t, conf, 4);
-        DSTR_STATIC(pattern, "\n");
-        PROP(&e, dstr_split(&conf_text, &pattern, &conf) );
-        // if there is a newline at the end of the file, ignore it
-        if(conf.len == 4 && conf.data[3].len == 0) conf.len--;
-        if(conf.len != 3){
-            ORIG(&e, E_INTERNAL, "config file must contain exactly the mysql "
-                    "host, user, and password, each on a separate line");
-        }
-        // get null-terminated values, for mysql api
-        PROP(&e, dstr_copy(&conf.data[0], &d_sqlhost) );
-        PROP(&e, dstr_copy(&conf.data[1], &d_sqluser) );
-        PROP(&e, dstr_copy(&conf.data[2], &d_sqlpass) );
-        PROP(&e, dstr_null_terminate(&d_sqlhost) );
-        PROP(&e, dstr_null_terminate(&d_sqluser) );
-        PROP(&e, dstr_null_terminate(&d_sqlpass) );
-        sqlhost = d_sqlhost.data;
-        sqluser = d_sqluser.data;
-        sqlpass = d_sqlpass.data;
-        sqlsock = "/var/run/mysqld/mysqld.sock";
+    // get USER variable (that is, to whom we are encrypting)
+    bool ok;
+    const dstr_t user = dgetenv(DSTR_LIT("USER"), &ok);
+    if(!ok){
+        ORIG(&e, E_INTERNAL, "no USER set");
     }
 
-    // init mysql
+    // get sql server ready
     int ret = mysql_library_init(0, NULL, NULL);
     if(ret != 0){
         ORIG(&e, E_SQL, "unable to init mysql library");
     }
 
-    // get a mysql object
     MYSQL sql;
     MYSQL* mret = mysql_init(&sql);
     if(!mret){
-        ORIG_GO(&e, E_SQL, "unable to init mysql object", cleanup_sql);
+        ORIG_GO(&e, E_SQL, "unable to init mysql object", cu_sql_lib);
     }
 
-    // make a connection with mysqld
-    mret = mysql_real_connect(&sql, sqlhost, sqluser, sqlpass,
-                "splintermail", 0, sqlsock, 0);
-    if(!mret){
-        trace_sql_error(&e, &sql);
-        ORIG_GO(&e, E_SQL, "unable to connect to mysqld", cleanup_sql_obj);
-    }
+    PROP_GO(&e, sql_connect_unix(&sql, NULL, NULL, sock), cu_sql);
 
-    // get the user id
-    unsigned int uid;
-    PROP_GO(&e, get_user_id(&sql, user, &uid), cleanup_sql_obj);
+    PROP_GO(&e, _mysql_encrypt(&sql, user), cu_sql);
 
-    // use the user_id to get the encryption keys
-    link_t keys;
-    link_init(&keys);
-    PROP_GO(&e, get_keys(&sql, uid, &keys), cu_keys);
-
-    if(link_list_isempty(&keys)){
-        TRACE(&e, "no keys for user %x\n", FS(user));
-        ORIG_GO(&e, E_NOKEYS, "No keys for user", cu_keys);
-    }
-
-    // ready to start encrypting
-    PROP_GO(&e, do_encryption(&keys), cu_keys);
-
-    link_t *link;
-cu_keys:
-    while((link = link_list_pop_first(&keys))){
-        keypair_t *kp = CONTAINER_OF(link, keypair_t, link);
-        keypair_free(&kp);
-    }
-cleanup_sql_obj:
+cu_sql:
     mysql_close(&sql);
-cleanup_sql:
+
+cu_sql_lib:
     mysql_library_end();
+
     return e;
 }
 
@@ -325,8 +221,8 @@ int main(int argc, char** argv){
     opt_spec_t o_debug = {'d', "debug", false, OPT_RETURN_INIT};
     opt_spec_t o_help = {'h', "help", false, OPT_RETURN_INIT};
 #ifdef BUILD_SERVER_CODE
-    opt_spec_t o_debug_sock = {'\0', "debug-sock", true, OPT_RETURN_INIT};
-    opt_spec_t* spec[] = {&o_debug, &o_help, &o_debug_sock};
+    opt_spec_t o_sock = {'s', "socket", true, OPT_RETURN_INIT};
+    opt_spec_t* spec[] = {&o_debug, &o_help, &o_sock};
 #else
     opt_spec_t* spec[] = {&o_debug, &o_help};
 #endif
@@ -344,7 +240,7 @@ int main(int argc, char** argv){
         printf("encrypt_msg: apply splintermail encryption to stdin\n");
         printf("usage: encrypt_msg KEY_FILE [...]\n");
 #ifdef BUILD_SERVER_CODE
-        printf("usage: USER=email encrypt_msg [--debug-sock SOCK]\n");
+        printf("usage: USER=email encrypt_msg [--socket SOCK]\n");
 #endif
         exit(0);
     }
@@ -362,11 +258,11 @@ int main(int argc, char** argv){
 
     if(newargc == 1){
 #ifdef BUILD_SERVER_CODE
-        const dstr_t *debug_sock = NULL;
-        if(o_debug_sock.found){
-            debug_sock = &o_debug_sock.val;
+        dstr_t *sock = &DSTR_LIT("/var/run/mysqld/mysqld.sock");
+        if(o_sock.found){
+            sock = &o_sock.val;
         }
-        PROP_GO(&e, mysql_encrypt(debug_sock), cleanup_ssl);
+        PROP_GO(&e, mysql_encrypt(sock), cleanup_ssl);
 #else
         ORIG_GO(&e, E_PARAM, "compiled without mysql support, please provide "
                          "encryption key files as command line arguments",
@@ -383,15 +279,13 @@ exit:
 #ifdef BUILD_SERVER_CODE
     // determine exit code, since we will DROP any error before exiting
     exitval = (is_error(e));
-    // any error at all (except for a user having no keys) is badbadbad
-    CATCH(e, E_NOKEYS){
-        // silently drop E_NOKEYS error, which is no error at all
-    }else CATCH(e, E_ANY){
+    // any error at all is badbadbad
+    CATCH(e, E_ANY){
         // write errors to logfile
         DUMP(e);
-        DSTR_STATIC(summary, "unexpected error in encrypt_message, check log");
+        DSTR_STATIC(summary, "error in encrypt_message");
         // badbadbad_alert will print to the log file directly
-        badbadbad_alert(&summary, NULL);
+        badbadbad_alert(&summary, &e.msg);
         // put a line break in log file for ease of reading
         LOG_ERROR("\n");
     }
--- test/enc_dec.py
+++ test/enc_dec.py
@@ -66,45 +66,28 @@ def test_file_enc_dec(enc, dec, test_files):
     assert payload == result, f"payload mismatch:\n{payload}\n  vs\n{result}"
 
 
-def test_server_enc_dec(enc, dec, test_files, script_runner):
+def test_server_enc_dec(enc, dec, test_files, smsql, sock):
 
     def readkey(keyfile):
         with open(os.path.join(test_files, "key_tool", keyfile)) as f:
             return f.read()
 
+    # create a temporary account
+    uuid = smsql.create_account("test@splintermail.com", "passwordpassword")
+
+    # populate some public keys
     privs = [readkey("key_%s.pem"%x) for x in "aemn"]
-
     pubs = [readkey("key_%s.pub"%x) for x in "aemn"]
-
-    def hexify(bstr):
-        return codecs.encode(bstr,'hex').decode('utf8')
-
-    fprs = [hexify(pysm.hash_key(k)) for k in pubs]
-
-    # populate the database with the keys we want
-    device_vals = [
-        "(0, \"%s\", \"%s\")"%(fpr, pub) for fpr, pub in zip(fprs, pubs)
-    ]
-    sql = """
-        delete from accounts;
-        insert into accounts (user_id, email, domain_id, password)
-        values (0, "test@splintermail.com", 0, "password");
-
-        delete from devices;
-
-        insert into devices (user_id, fingerprint, public_key)
-        values %s;
-    """%(", ".join(device_vals))
-    script_runner.run(sql, "splintermail")
+    fprs = [smsql.add_device(uuid, pub) for pub in pubs]
 
     payload = base64.b64encode(os.urandom(10000))
 
     # encrypt the message once to all keys in the database
     enc_proc = subprocess.Popen(
-        [enc, "--debug-sock", script_runner.sockpath],
+        [enc, "--socket", sock],
         stdin=subprocess.PIPE,
         stdout=subprocess.PIPE,
-        env={"USER": "test@splintermail.com"},
+        env={"USER": pysm.to_fsid(uuid) + "@x.splintermail.com"},
     )
 
     reader = ReaderThread(enc_proc.stdout)
@@ -138,7 +121,37 @@ def test_server_enc_dec(enc, dec, test_files, script_runner):
         assert ret == 0, f"decrypt_msg exited {ret}"
 
         result = reader.get_data()
-        assert payload == result, f"payload mismatch:\n{payload}\n  vs\n{result}"
+        assert payload == result, (
+            f"payload mismatch:\n{payload}\n  vs\n{result}"
+        )
+
+
+def test_server_enc_dec_nokeys(enc, smsql, sock):
+    # Test again without any keys
+    uuid = smsql.create_account("nokeys@splintermail.com", "passwordpassword")
+
+    payload = base64.b64encode(os.urandom(10000))
+
+    # encrypt the message once to all keys in the database
+    enc_proc = subprocess.Popen(
+        [enc, "--socket", sock],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        env={"USER": pysm.to_fsid(uuid) + "@x.splintermail.com"},
+    )
+
+    reader = ReaderThread(enc_proc.stdout)
+    reader.start()
+
+    enc_proc.stdin.write(payload)
+    enc_proc.stdin.close()
+
+    ret = enc_proc.wait()
+    assert ret == 0, f"encrypt_msg exited {ret}"
+
+    # get the (un)encrypted message
+    msg = reader.get_data()
+    assert msg == payload
 
 
 if __name__ == "__main__":
@@ -157,11 +170,14 @@ if __name__ == "__main__":
     if "--server" in sys.argv:
         import mariadb
         # TODO: fix this
-        pysm_path = "./server"
+        pysm_path = "./server/pysm"
         sys.path.append(pysm_path)
         import pysm
         migmysql_path = "./server/migmysql"
         with mariadb.mariadb(None, migrations, migmysql_path) as script_runner:
-            test_server_enc_dec(enc, dec, test_files, script_runner)
+            sock = script_runner.sockpath
+            with pysm.SMSQL(sock=sock) as smsql:
+                test_server_enc_dec(enc, dec, test_files, smsql, sock)
+                test_server_enc_dec_nokeys(enc, smsql, sock)
 
     print("PASS")
