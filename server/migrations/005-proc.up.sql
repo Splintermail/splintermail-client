103ca039a7ff323557acd28efe7001cbcac886d1
Splintermail Dev <dev@splintermail.com>
2021-04-07 08:27:48 -0600

    migrations: actually, use hex for TO_FSID

--- server/migrations/005-proc.up.sql
+++ server/migrations/005-proc.up.sql
@@ -1,7 +1,7 @@
 -- create stored functions for converting to/from binary uuids and
--- file-system-safe identifiers
-create function TO_FSID (user_uuid binary(32)) returns char(44) deterministic
-    return REPLACE(TO_BASE64(user_uuid), '/', '-');
+-- file-system-safe identifiers (at this time, it's just hex encoding)
+create function TO_FSID (user_uuid binary(32)) returns char(64) deterministic
+    return LOWER(HEX(user_uuid));
 
-create function TO_UUID (fsid char(44)) returns binary(32) deterministic
-    return FROM_BASE64(REPLACE(fsid, '-', '/'));
+create function TO_UUID (fsid char(64)) returns binary(32) deterministic
+    return UNHEX(fsid);
