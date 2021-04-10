21f18a34d0734e581807c2b2bc361504aeb7241f
Splintermail Dev <dev@splintermail.com>
2021-04-10 12:41:36 -0600

    wait, no, just fix the fsids a bit more
    Really, 64-characters is just too long for the localpart of the email.
    So many log lines in mail.log get truncated!

--- server/libsmsql.c
+++ server/libsmsql.c
@@ -2,16 +2,72 @@
 
 // helper functions
 
+static derr_type_t to_fsid_quiet(const dstr_t *uuid, dstr_t *out){
+    // use a local buffer so bin2b64 doesn't write an extra byte
+    DSTR_VAR(buf, SMSQL_FSID_SIZE);
+
+    derr_type_t type = bin2b64_quiet(uuid, &buf, 0, true, NULL);
+    if(type) return type;
+    // replace '/' with '-' to be filesystem safe
+    for(size_t i = 0; i < buf.len; i++){
+        if(buf.data[i] == '/') buf.data[i] = '-';
+    }
+
+    // write the 'F' prefix to ensure the output is email-localpart-safe
+    type = dstr_append_quiet(out, &DSTR_LIT("F"));
+    if(type) return type;
+
+    // write all but the final '=' of the output
+    dstr_t sub = dstr_sub2(buf, 0, buf.len - 1);
+    type = dstr_append_quiet(out, &sub);
+    if(type) return type;
+
+    return E_NONE;
+}
+
+static derr_type_t to_uuid_quiet(const dstr_t *fsid, dstr_t *out){
+    // make a copy so we don't violate the (implicit) const input constraint
+    DSTR_VAR(b64, SMSQL_FSID_SIZE);
+
+    // ignore the initial 'F'
+    dstr_t sub = dstr_sub2(*fsid, 1, fsid->len);
+    derr_type_t type = dstr_append_quiet(&b64, &sub);
+    if(type) return type;
+
+    // append the final "="
+    type = dstr_append_quiet(&b64, &DSTR_LIT("="));
+    if(type) return type;
+
+    // replace '-' with '/' to be valid base64
+    for(size_t i = 0; i < b64.len; i++){
+        if(b64.data[i] == '-') b64.data[i] = '/';
+    }
+    type = b642bin_quiet(&b64, out, NULL);
+    if(type) return type;
+
+    return E_NONE;
+}
+
 derr_t to_fsid(const dstr_t *uuid, dstr_t *out){
     derr_t e = E_OK;
-    PROP(&e, bin2hex(uuid, out) );
+    derr_type_t type = to_fsid_quiet(uuid, out);
+    if(type) ORIG(&e, type, "output error");
     return e;
 }
 
-derr_t to_uuid(const dstr_t *fsid, dstr_t *out){
+derr_t to_uuid(const dstr_t *uuid, dstr_t *out){
     derr_t e = E_OK;
-    PROP(&e, hex2bin(fsid, out) );
+    derr_type_t type = to_uuid_quiet(uuid, out);
+    if(type) ORIG(&e, type, "output error");
     return e;
+}
+
+derr_type_t fmthook_fsid(dstr_t* out, const void* arg){
+    // cast the input
+    const dstr_t* uuid = (const dstr_t*)arg;
+    derr_type_t type = to_fsid_quiet(uuid, out);
+    if(type) return type;
+    return E_NONE;
 }
 
 // predefined queries
--- server/libsmsql.h
+++ server/libsmsql.h
@@ -9,20 +9,26 @@
 derr_t to_fsid(const dstr_t *uuid, dstr_t *out);
 derr_t to_uuid(const dstr_t *fsid, dstr_t *out);
 
-// FSID is secretly an alias for FX.  Someday we could potentially migrate
-// FSID encoding schemes so both APIs remain.
-#define FSID FX
+derr_type_t fmthook_fsid(dstr_t* out, const void* arg);
+
+static inline fmt_t FSID(const dstr_t* uuid){
+    return (fmt_t){FMT_EXT, {.ext = {.arg = (const void*)uuid,
+                                     .hook = fmthook_fsid} } };
+}
 
 // predefined queries
 
 #define SMSQL_UUID_SIZE 32
-/* FSID was once a modified base64 to be filesystem-safe, but it caused too
-   many issues; first postfix would sometimes lower-case the base64, and also
-   it would puke if the base64 began with a punctuation (emails aren't supposed
-   to).  Those are both fixable, but saving 20 characters of text is not worth
-   the complexity required, or the risk of finding other pitfalls so now the
-   fsid is just a lower-case hex id, which can't possibly fail. */
-#define SMSQL_FSID_SIZE 64
+/* FSID is a modified base64 to be filesystem-safe, and also to be
+   email-localpart-safe.  This is done by converting '/' into '-', prefixing
+   with an F to ensure the address never starts with punctuation, and removing
+   the training "=" to shorten things.  Postfix also must be configured to not
+   case-fold the localpart when it invokes dovecot-lda.
+
+   This is a lot of complexity to shorten the uuid email address, but when I
+   tried using hex-encoding (~20 extra characters) way too many log messages
+   got truncated and it was hard to debug the system. */
+#define SMSQL_FSID_SIZE 44
 #define SMSQL_EMAIL_SIZE 100
 
 derr_t get_uuid_for_email(
