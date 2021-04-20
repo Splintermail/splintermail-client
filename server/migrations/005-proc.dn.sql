0511d7077f2757a6454b520877d766ee536c1bd6
Splintermail Dev <dev@splintermail.com>
2021-04-20 07:55:28 -0600

    migrations: nvm, use improved b64-based FSIDs

--- server/migrations/005-proc.dn.sql
+++ server/migrations/005-proc.dn.sql
@@ -1,2 +1,3 @@
 drop function TO_FSID;
 drop function TO_UUID;
+drop function TO_UUID_SOFT;
--- server/migrations/005-proc.up.sql
+++ server/migrations/005-proc.up.sql
@@ -1,7 +1,20 @@
 -- create stored functions for converting to/from binary uuids and
--- file-system-safe identifiers (at this time, it's just hex encoding)
-create function TO_FSID (user_uuid binary(32)) returns char(64) deterministic
-    return LOWER(HEX(user_uuid));
+-- file-system-safe identifiers
+create function TO_FSID (user_uuid binary(32)) returns char(44) DETERMINISTIC
+    return CONCAT("F", TRIM(TRAILING "=" FROM REPLACE(TO_BASE64(user_uuid), '/', '-')));
 
-create function TO_UUID (fsid char(64)) returns binary(32) deterministic
-    return UNHEX(fsid);
+create function TO_UUID (fsid char(44)) returns binary(32) DETERMINISTIC
+    return FROM_BASE64(REPLACE(CONCAT(TRIM(LEADING "F" FROM fsid), "="), '-', '/'));
+
+-- If you need to convert an arbitrary string which *might* be an fsid.
+-- The SQLEXCEPTION guards against bad base64 strings.
+-- We re-embed the whole function body without the char(44) limitation to
+-- handle strings which are too long.
+create function TO_UUID_SOFT (fsid TEXT) returns binary(32) DETERMINISTIC
+    BEGIN
+        DECLARE EXIT HANDLER FOR SQLEXCEPTION, 1406
+        BEGIN
+            return NULL;
+        END;
+        return FROM_BASE64(REPLACE(CONCAT(TRIM(LEADING "F" FROM SUBSTRING(fsid FROM 1 FOR 44)), "="), '-', '/'));
+    END;
