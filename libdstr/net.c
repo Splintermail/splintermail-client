2ac9fa81ffc801c128042d072c5263a9a82c5051
Splintermail Dev <dev@splintermail.com>
2022-07-22 08:33:40 -0600

    add read_addr to libdstr/net.c

--- libdstr/net.c
+++ libdstr/net.c
@@ -1,3 +1,6 @@
+#include <string.h>
+#include <errno.h>
+
 #include "libdstr.h"
 
 derr_type_t fmthook_ntop4(dstr_t* out, const void* arg){
@@ -35,4 +38,50 @@ derr_type_t fmthook_ntop(dstr_t* out, const void* arg){
         return fmthook_ntop4(out, (const struct sockaddr_in6*)sa);
     }
     return E_PARAM;
+}
+
+derr_t read_addr(struct sockaddr_storage *ss, const char *addr, uint16_t port){
+    derr_t e = E_OK;
+
+    *ss = (struct sockaddr_storage){0};
+
+    // detect ipv4 vs ipv6
+    sa_family_t family = AF_INET;
+    size_t addrlen = strlen(addr);
+    for(size_t i = 0; i < addrlen; i++){
+        if(addr[i] == ':'){
+            family = AF_INET6;
+            break;
+        }
+    }
+
+    if(family == AF_INET){
+        struct sockaddr_in sin = {0};
+        sin.sin_family = family;
+        sin.sin_port = htons(port);
+        struct in_addr in_addr;
+        int ret = inet_pton(family, addr, &in_addr);
+        if(ret != 1){
+            TRACE(&e, "unable to parse address: %x\n", FE(&errno));
+            ORIG(&e, E_PARAM, "unable to parse address");
+        }
+        sin.sin_addr = in_addr;
+        struct sockaddr_in *out = (struct sockaddr_in*)ss;
+        *out = sin;
+    }else{
+        struct sockaddr_in6 sin6 = {0};
+        sin6.sin6_family = family;
+        sin6.sin6_port = htons(port);
+        struct in6_addr in6_addr;
+        int ret = inet_pton(family, addr, &in6_addr);
+        if(ret != 1){
+            TRACE(&e, "unable to parse address: %x\n", FE(&errno));
+            ORIG(&e, E_PARAM, "unable to parse address");
+        }
+        sin6.sin6_addr = in6_addr;
+        struct sockaddr_in6 *out = (struct sockaddr_in6*)ss;
+        *out = sin6;
+    }
+
+    return e;
 }
--- libdstr/net.h
+++ libdstr/net.h
@@ -5,6 +5,12 @@ static inline fmt_t FNTOP(const struct sockaddr *arg){
                                      .hook = fmthook_ntop} } };
 }
 
+// sockaddr_storage version; still calls fmthook_ntop
+static inline fmt_t FNTOPS(const struct sockaddr *arg){
+    return (fmt_t){FMT_EXT, {.ext = {.arg = (const void*)arg,
+                                     .hook = fmthook_ntop} } };
+}
+
 derr_type_t fmthook_ntop4(dstr_t* out, const void* arg);
 
 static inline fmt_t FNTOP4(const struct sockaddr_in *arg){
@@ -18,3 +24,5 @@ static inline fmt_t FNTOP6(const struct sockaddr_in6 *arg){
     return (fmt_t){FMT_EXT, {.ext = {.arg = (const void*)arg,
                                      .hook = fmthook_ntop6} } };
 }
+
+derr_t read_addr(struct sockaddr_storage *ss, const char *addr, uint16_t port);
